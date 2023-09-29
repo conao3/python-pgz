9fe597d4a554397a9c8184fb047c20c5e703d371
Naoya Yamashita <conao3@gmail.com>
2023-09-30 00:00:10 +0900

    implement locate_dominating_file

--- src/pgz/lib.py
+++ src/pgz/lib.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import Optional
 
 
 def create_repo_file(gitdir: pathlib.Path, filename: str) -> pathlib.Path:
@@ -14,3 +15,14 @@ def create_repo_dir(gitdir: pathlib.Path, dirname: str) -> pathlib.Path:
     dirpath.mkdir(parents=True, exist_ok=True)
 
     return dirpath
+
+
+def locate_dominating_file(path: pathlib.Path, filename: str) -> Optional[pathlib.Path]:
+    path = path.resolve()
+
+    while not (path / filename).exists():
+        if path.parent == path:
+            return None
+        path = path.parent
+
+    return path / filename
